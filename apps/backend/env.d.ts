0ccb835039a214f2587d1c3658a540dade42292e
Joao1V <joaovm.dev@gmail.com>
2025-11-02 18:33:57 -0400

    feat(env): add public environment variables to backend config

--- apps/backend/env.d.ts
+++ apps/backend/env.d.ts
@@ -1,20 +1,24 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    // Database
-    DATABASE_URI: string;
-    PAYLOAD_SECRET: string;
+   interface ProcessEnv {
+      //Next Public
+      NEXT_PUBLIC_SERVER_URL: string;
+      NEXT_PUBLIC_FRONTEND_URL: string;
 
-    // Email Configuration
-    MAIL_HOST: string;
-    MAIL_PORT: string;
-    MAIL_USER: string;
-    MAIL_PASS: string;
+      // Database
+      DATABASE_URI: string;
+      PAYLOAD_SECRET: string;
 
-    // AWS S3 Storage
-    S3_BUCKET: string;
-    S3_REGION: string;
-    S3_ACCESS_KEY_ID: string;
-    S3_SECRET_ACCESS_KEY: string;
-    S3_ENDPOINT: string;
-  }
+      // Email Configuration
+      MAIL_HOST: string;
+      MAIL_PORT: string;
+      MAIL_USER: string;
+      MAIL_PASS: string;
+
+      // AWS S3 Storage
+      S3_BUCKET: string;
+      S3_REGION: string;
+      S3_ACCESS_KEY_ID: string;
+      S3_SECRET_ACCESS_KEY: string;
+      S3_ENDPOINT: string;
+   }
 }
