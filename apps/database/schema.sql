ee953650545d8ff1e12e868f24757a905302c856
Gabi <bibi.barbosa.martins@hotmail.com>
2025-11-04 13:29:17 -0400

    Add `users`, `products`, and `locations` tables to database schema

--- apps/database/schema.sql
+++ apps/database/schema.sql
@@ -21,3 +21,59 @@ alter type enum_users_role owner to postgres;
 create type enum_animals_species as enum ('dog', 'cat');
 
 alter type enum_animals_species owner to postgres;
+
+create table users
+(
+    id                        serial
+        primary key,
+    updated_at                timestamp(3) with time zone default now() not null,
+    created_at                timestamp(3) with time zone default now() not null,
+    email                     varchar                                   not null,
+    reset_password_token      varchar,
+    reset_password_expiration timestamp(3) with time zone,
+    salt                      varchar,
+    hash                      varchar,
+    login_attempts            numeric                     default 0,
+    lock_until                timestamp(3) with time zone,
+    name                      varchar                                   not null,
+    phone                     varchar,
+    api_key                   varchar,
+    api_key_index             varchar,
+    role                      enum_users_role             default 'user'::enum_users_role,
+    enable_a_p_i_key          boolean,
+    birth_date                timestamp(3) with time zone
+);
+
+alter table users
+    owner to postgres;
+
+
+create table products
+(
+    id         serial
+        primary key,
+    name       varchar                                   not null,
+    price      numeric                                   not null,
+    updated_at timestamp(3) with time zone default now() not null,
+    created_at timestamp(3) with time zone default now() not null
+);
+
+alter table products
+    owner to postgres;
+
+
+create table locations
+(
+    id          serial
+        primary key,
+    district    varchar,
+    city        varchar                                   not null,
+    state       varchar                                   not null,
+    coordinates geometry(Point),
+    updated_at  timestamp(3) with time zone default now() not null,
+    created_at  timestamp(3) with time zone default now() not null,
+    street      varchar                                   not null
+);
+
+alter table locations
+    owner to postgres;
